aa7c480343fca98b0f73c7e1d5d4d1993cbb6b2c
devashanv <ashanwithanarachchi@gmail.com>
2024-09-12 15:38:10 -0500

    add day03 challenge

---
+++ Day-03/script.js
@@ -0,0 +1,13 @@
+
+let popupCard = document.getElementById("_popupcard");
+
+//popup open fn
+function displayPopup(){
+    popupCard.classList.add("card-popup-open");
+}
+
+//popup close fn
+function closePopup(){
+    popupCard.classList.remove("card-popup-open");
+}
+
