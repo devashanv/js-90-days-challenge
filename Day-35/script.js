073fbe86433197b6cd1f24bcce818468c684e1bd
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2024-10-14 22:05:26 +0530

    add mouse touchmove event to navigate cursor object.

--- Day-35/script.js
+++ Day-35/script.js
@@ -1,6 +1,8 @@
 //initialize
 const cursor = document.getElementById("cursor");
 
+console.log(cursor)
+
 //create touch event
 const touchEvent = () => {
     document.createEvent("TouchEvent");
@@ -9,11 +11,20 @@ const touchEvent = () => {
 //navigate cursor
 const navigate = (eve) => {
     //get cursor position
-    var axisX = touchEvent() ? eve.pageX : eve.touches[0].pageX;
-    var axisY = touchEvent() ? eve.pageY : eve.touches[0].pageY;
+    var axisX = !touchEvent() ? eve.pageX : eve.touches[0].pageX;
+    var axisY = !touchEvent() ? eve.pageY : eve.touches[0].pageY;
 
     //set "cursor" object to page cursor
-    cursor.style.left = `${axisX}px`;
-    cursor.style.top = `${axisY}px`;
+    cursor.style.left = `${axisX - 20}px`;
+    cursor.style.top = `${axisY - 20}px`;
+};
 
-}
+//handle mousemove with navigate cursor
+document.addEventListener("mousemove", (eve) => {
+    navigate(eve);
+});
+
+//handle touch with navigate
+document.addEventListener("touchmove", (eve) =>{
+    navigate(eve);
+});
